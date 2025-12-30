25f66c072372b077f7a3c7786d7fc1067a844e30
MFlisar <mflisar.development@gmail.com>
2025-12-30 16:30:23 +0100

    ...

--- library/plugins/build-gradle/src/main/java/com/michaelflisar/kmplibrary/readme/UpdateReadmeUtil.kt
+++ library/plugins/build-gradle/src/main/java/com/michaelflisar/kmplibrary/readme/UpdateReadmeUtil.kt
@@ -1,8 +1,192 @@
 package com.michaelflisar.kmplibrary.readme
 
+import com.michaelflisar.kmplibrary.configs.Config
+import com.michaelflisar.kmplibrary.configs.LibraryConfig
 import java.io.File
 
 object UpdateReadmeUtil {
+
+    fun update(
+        projectDirectory: File,
+        config: Config,
+        libraryConfig: LibraryConfig
+    ) {
+        println("")
+        println("#####################################")
+        println("###   BEGIN updateMarkdownFiles   ###")
+        println("#####################################")
+        println("")
+
+        // files
+        val fileAppVersionToml = File(projectDirectory, "gradle/app.versions.toml")
+        val fileReadmeTemplate = File(projectDirectory, "tooling/docs/readme.md")
+        val fileReadme = File(projectDirectory, "README.md")
+        val folderDocumentation = File(projectDirectory, "documentation")
+        val folderDocumentationModules = File(projectDirectory, "documentation/modules")
+        val folderDocumentationScreenshots = File(projectDirectory, "documentation/screenshots")
+
+        // load data from project files
+        val minSdk =
+            UpdateReadmeUtil.readTOMLProperty(fileAppVersionToml, "versions", "minSdk").toInt()
+        val supportedPlatforms = libraryConfig.modules.map { module ->
+            val platforms =
+                UpdateReadmeUtil.getSupportedPlatformsFromModule(File(projectDirectory, "${module.path}/build.gradle.kts"))
+            module to platforms
+        }
+        val allSupportedPlatforms = supportedPlatforms.map { it.second }.flatten().distinct()
+        val allSupportedPlatformsLowercase = allSupportedPlatforms.map { it.lowercase() }
+
+        val isAndroidSupported = allSupportedPlatformsLowercase.contains("android")
+        val isIosSupported = allSupportedPlatformsLowercase.contains("ios")
+        val isWindowsSupported = allSupportedPlatformsLowercase.contains("windows")
+        val isMacOsSupported = allSupportedPlatformsLowercase.contains("macos")
+        val isLinuxSupported = allSupportedPlatformsLowercase.contains("linux")
+        val isWasmSupported = allSupportedPlatformsLowercase.contains("wasm")
+        val isJsSupported = allSupportedPlatformsLowercase.contains("js")
+
+        // 1) load all markdown files from the documentation folder (excluding _partials)
+        val markdownTitleRegex = Regex("^#\\s+(.*)$", RegexOption.MULTILINE)
+        //val pathDocumentation = "documentation"
+        //val pathModules = "documentation/modules"
+        val markdownFilesWithName = folderDocumentation
+            .walkTopDown()
+            .filter { it.isFile && it.extension == "md" }
+            .filter { !it.path.contains("_partials") }
+            .toList()
+            .sortedBy { it.path }
+            .map {
+                val content = it.readText()
+                // extract name from first markdown header
+                val nameMatch = markdownTitleRegex.find(content)
+                val name = nameMatch?.groups?.get(1)?.value?.trim() ?: it.nameWithoutExtension
+                it to name
+            }
+
+        // 2) get all modules and other markdown files as links
+        val moduleLinks = markdownFilesWithName
+            .filter { it.first.startsWith(folderDocumentationModules) }
+            .map {
+                val relativePath = it.first.relativeTo(projectDirectory).path.replace("\\", "/")
+                val (path, name) = it
+                "- [$name]($relativePath)"
+            }
+        val otherLinks = markdownFilesWithName
+            .filter { !it.first.startsWith(folderDocumentationModules) }
+            .map {
+                val relativePath = it.first.relativeTo(projectDirectory).path.replace("\\", "/")
+                val (path, name) = it
+                "- [$name]($relativePath)"
+            }
+
+        // 3) create header replacement
+        val imageMavenCentral = ReadmeDefaults.imageMavenCentral(libraryConfig)
+        val imageAPI = ReadmeDefaults.imageAPI(minSdk)
+        val imageKotlin = ReadmeDefaults.imageKotlin(libraryConfig)
+        val imageKMP = ReadmeDefaults.imageKMP()
+        val imageLicence = ReadmeDefaults.imageLicence(libraryConfig)
+
+        val headerLine1 = "$imageMavenCentral $imageAPI $imageKotlin $imageKMP $imageLicence"
+        val headerLine2 = "# ${libraryConfig.library.name}"
+        val headerLine3 = listOfNotNull(
+            if (isAndroidSupported) ReadmeDefaults.ImageAndroid else null,
+            if (isIosSupported) ReadmeDefaults.ImageIOS else null,
+            if (isWindowsSupported) ReadmeDefaults.ImageWindows else null,
+            if (isMacOsSupported) ReadmeDefaults.ImageMacOS else null,
+            if (isLinuxSupported) ReadmeDefaults.ImageLinux else null,
+            if (isWasmSupported) ReadmeDefaults.ImageWASM else null,
+            if (isJsSupported) ReadmeDefaults.ImageJS else null,
+        )
+            .joinToString(" ")
+        val header = listOf(
+            headerLine1,
+            headerLine2,
+            headerLine3
+        ).joinToString("\n")
+
+        // 4) create supported platforms table
+        val supportedPlatformsTable = buildString {
+            val header = listOf("Module") + allSupportedPlatforms
+            appendLine("| " + header.joinToString(" | ") + " |")
+            appendLine("|" + header.joinToString("|") { "---" } + "|")
+            for ((module, platforms) in supportedPlatforms) {
+                val row = listOf(module.name) + allSupportedPlatformsLowercase.map { platform ->
+                    if (platforms.map { it.lowercase() }
+                            .contains(platform.lowercase())) "✅" else "❌"
+                }
+                appendLine("| " + row.joinToString(" | ") + " |")
+            }
+        }
+
+        // 5) create setup instructions
+        val libraryName = libraryConfig.library.name.lowercase().replace(" ", "-")
+        val setupViaDependencies = buildString {
+            appendLine("val $libraryName = \"<LATEST-VERSION>\"")
+            appendLine()
+            for (module in libraryConfig.modules) {
+                appendLine("implementation(\"${libraryConfig.maven.groupId}:${module.artifactId}:\${$libraryName}\")")
+            }
+        }
+
+        val setupViaVersionCatalogue1 = buildString {
+            appendLine("[versions]")
+            appendLine()
+            appendLine("$libraryName = \"<LATEST-VERSION>\"")
+            appendLine()
+            appendLine("[libraries]")
+            appendLine()
+            for (module in libraryConfig.modules) {
+                appendLine(
+                    "${
+                        module.artifactId.replace(
+                            "-",
+                            "."
+                        )
+                    } = { module = \"${libraryConfig.maven.groupId}:${module.artifactId}\", version.ref = \"$libraryName\" }"
+                )
+            }
+        }
+
+        val setupViaVersionCatalogue2 = buildString {
+            for (module in libraryConfig.modules) {
+                appendLine("implementation(libs.${module.artifactId.replace("-", ".")})")
+            }
+        }
+
+        // 6) create screenshot replacement
+        val screenshots = folderDocumentationScreenshots.listFiles().map {
+            val relativePath = it.relativeTo(projectDirectory).path.replace("\\", "/")
+            "![${it.nameWithoutExtension}]($relativePath)"
+        }
+
+        // 2) read template content
+        var readmeContent = fileReadmeTemplate.readText()
+
+        // 3) replace placeholders in readme with content from markdown files
+        val replacements = listOf(
+            Placeholder("{{ header }}", header),
+            Partial("{{ partials.introduction }}", File(projectDirectory, "documentation/_partials/introduction.md")),
+            Partial("{{ partials.features }}", File(projectDirectory, "documentation/_partials/features.md")),
+            Partial("{{ partials.usage }}", File(projectDirectory, "documentation/_partials/usage.md")),
+            Placeholder("{{ modules }}", moduleLinks.joinToString("\n")),
+            Placeholder("{{ links }}", otherLinks.joinToString("\n")),
+            Placeholder("{{ supported_platforms }}", supportedPlatformsTable),
+            Placeholder("{{ setup-via-dependencies }}", setupViaDependencies),
+            Placeholder("{{ setup-via-version-catalogue1 }}", setupViaVersionCatalogue1),
+            Placeholder("{{ setup-via-version-catalogue2 }}", setupViaVersionCatalogue2),
+            Placeholder("{{ screenshots }}", screenshots.joinToString("\n")),
+        )
+        for (replacement in replacements) {
+            readmeContent = replacement.replace(readmeContent)
+        }
+
+        // 4) write updated readme content to README.md
+        fileReadme.writeText(readmeContent)
+
+        println("")
+        println("#####################################")
+        println("###   END updateMarkdownFiles     ###")
+        println("#####################################")
+    }
 
     /**
      * Returns a Markdown string for an image, optionally wrapped in a link.
