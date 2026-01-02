4dc3ebcba9d934f6ec1d43be95882dcaf9ea56b3
MFlisar <mflisar.development@gmail.com>
2026-01-02 18:19:39 +0100

    ...

--- library/core/src/main/java/com/michaelflisar/kmplibrary/core/Util.kt
+++ library/core/src/main/java/com/michaelflisar/kmplibrary/core/Util.kt
@@ -4,45 +4,6 @@ import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.DeserializationStrategy
 import java.io.File
 
-fun rootFolder() = File(System.getProperty("user.dir"))
-
-fun File.update(oldString: String, newString: String) {
-    val content = readText()
-    val updatedContent = content.replace(oldString, newString)
-    if (content != updatedContent) {
-        writeText(updatedContent)
-        println("Updated file: $this")
-    }
-}
-
-fun deleteEmptyDirs(folder: File) {
-    folder.walkBottomUp()
-        .filter { it.isDirectory && it.listFiles()?.isEmpty() == true }
-        .forEach { it.delete() }
-}
-
-fun File.walkTopDownFiltered(filter: (File) -> Boolean = { true }): Sequence<File> {
-    return this.walkTopDown().filter(filter)
-}
-
-fun File.saveDeleteRecursively() {
-    if (exists()) {
-        deleteRecursively()
-    }
-}
-
-fun File.saveDelete() {
-    if (exists()) {
-        delete()
-    }
-}
-
-fun File.deleteIfEmpty() {
-    if (isDirectory && listFiles()?.isEmpty() == true) {
-        delete()
-    }
-}
-
 fun <T> Yaml.Companion.tryRead(file: File, deserializer: DeserializationStrategy<T>): T? {
     return try {
         val content = file.readText(Charsets.UTF_8)
@@ -52,12 +13,18 @@ fun <T> Yaml.Companion.tryRead(file: File, deserializer: DeserializationStrategy
     }
 }
 
-inline fun <reified T> Yaml.Companion.read(file: File, deserializer: DeserializationStrategy<T>): T {
+inline fun <reified T> Yaml.Companion.read(
+    file: File,
+    deserializer: DeserializationStrategy<T>,
+): T {
     return try {
         val content = file.readText(Charsets.UTF_8)
         Yaml.default.decodeFromString(deserializer, content)
     } catch (e: Exception) {
         e.printStackTrace()
-        throw RuntimeException("Failed to read `${T::class.simpleName}` from path '${file.path}'", e)
+        throw RuntimeException(
+            "Failed to read `${T::class.simpleName}` from path '${file.path}'",
+            e
+        )
     }
 }
