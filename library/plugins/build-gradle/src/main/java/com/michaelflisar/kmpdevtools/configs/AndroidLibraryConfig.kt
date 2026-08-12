eff560ea339de358c6db5bd9f827689bf983385f
MFlisar <mflisar.development@gmail.com>
2026-08-12 11:14:14 +0200

    AndroidLibraryConfig::createFromPath does support external modules now

--- library/plugins/build-gradle/src/main/java/com/michaelflisar/kmpdevtools/configs/AndroidLibraryConfig.kt
+++ library/plugins/build-gradle/src/main/java/com/michaelflisar/kmpdevtools/configs/AndroidLibraryConfig.kt
@@ -1,5 +1,6 @@
 package com.michaelflisar.kmpdevtools.configs
 
+import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 
 class AndroidLibraryConfig private constructor(
@@ -33,16 +34,17 @@ class AndroidLibraryConfig private constructor(
             compileSdk: Provider<String>,
             minSdk: Provider<String>,
             enableAndroidResources: Boolean = true,
+            customNamespaceAddon: String? = null
         ): AndroidLibraryConfig {
-            val relativePath =
-                libraryModuleConfig.project.projectDir.relativeTo(libraryModuleConfig.project.rootDir).invariantSeparatorsPath
-            val namespace = relativePath.split("/").joinToString(".")
-            val androidNamespace = "${libraryModuleConfig.projectNamespace}.$namespace"
             return AndroidLibraryConfig(
                 compileSdk = compileSdk,
                 minSdk = minSdk,
                 enableAndroidResources = enableAndroidResources,
-                namespace = androidNamespace
+                namespace = createNamespaceFromPath(
+                    libraryModuleConfig.project,
+                    libraryModuleConfig.projectNamespace,
+                    customNamespaceAddon
+                )
             )
         }
 
@@ -51,31 +53,50 @@ class AndroidLibraryConfig private constructor(
             compileSdk: Provider<String>,
             minSdk: Provider<String>,
             enableAndroidResources: Boolean = true,
+            customNamespaceAddon: String? = null
         ): AndroidLibraryConfig {
-            val relativePath =
-                appModuleConfig.project.projectDir.relativeTo(appModuleConfig.project.rootDir).invariantSeparatorsPath
-            val namespace = relativePath.split("/").joinToString(".")
-            val androidNamespace = "${appModuleConfig.projectNamespace}.$namespace"
             return AndroidLibraryConfig(
                 compileSdk = compileSdk,
                 minSdk = minSdk,
                 enableAndroidResources = enableAndroidResources,
-                namespace = androidNamespace
+                namespace = createNamespaceFromPath(
+                    appModuleConfig.project,
+                    appModuleConfig.projectNamespace,
+                    customNamespaceAddon
+                )
             )
         }
 
-        fun createManual(
-            libraryModuleConfig: LibraryModuleConfig.Library,
-            compileSdk: Provider<String>,
-            minSdk: Provider<String>,
-            namespaceAddon: String
-        ): AndroidLibraryConfig {
-            return AndroidLibraryConfig(
-                compileSdk = compileSdk,
-                minSdk = minSdk,
-                enableAndroidResources = true,
-                namespace = "${libraryModuleConfig.projectNamespace}.$namespaceAddon"
-            )
+        private fun createNamespaceFromPath(
+            project: Project,
+            projectNamespace: String,
+            customNamespaceAddon: String?
+        ): String {
+
+            if (!customNamespaceAddon.isNullOrBlank()) {
+                return "$projectNamespace.$customNamespaceAddon"
+            }
+
+            val namespace = project.rootDir
+                .toPath()
+                .toAbsolutePath()
+                .normalize()
+                .relativize(
+                    project.projectDir
+                        .toPath()
+                        .toAbsolutePath()
+                        .normalize()
+                )
+                .filter { it.toString() != ".." }
+                .joinToString(".") {
+                    it.toString().replace("-", "_")
+                }
+
+            return if (namespace.isBlank()) {
+                projectNamespace
+            } else {
+                "$projectNamespace.$namespace"
+            }
         }
     }
 }
